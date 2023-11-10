673da7ec5a13e20fd4fc9c1e870ac503fbe69eab
ekovv <denisovdima2004@yandex.ru>
2023-11-10 16:20:54 +0300

    GOMARKET-10 :sparkles: storage test ✅

--- internal/repository/user_repository_test.go
+++ internal/repository/user_repository_test.go
@@ -37,29 +37,30 @@ func Test_userRepository_CreateUser(t *testing.T) {
 			name: "BAD",
 			args: args{
 				user: model.User{
-					Username: "dima",
+					Username: "anton",
 				},
 			},
 			wantErr: invalidErr,
 		},
 	}
+
+	ctx := context.TODO()
+	vdb, err := dockerdb.New(ctx, dockerdb.PostgresConfig("market").Build())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer vdb.Clear(ctx)
+
+	gormDB := db.NewDB(config.Config{DB: vdb.GetSQLConnStr()})
+	if err != nil {
+		t.Fatal(err)
+	}
+	db := userRepository{
+		db: gormDB,
+	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ctx := context.TODO()
-			vdb, err := dockerdb.New(ctx, dockerdb.PostgresConfig("market").Build())
-			if err != nil {
-				t.Fatal(err)
-			}
-			defer vdb.Clear(ctx)
-
-			//gormDB, err := gorm.Open(postgres.Open(vdb.GetSQLConnStr()), &gorm.Config{})
-			gormDB := db.NewDB(config.Config{DB: vdb.GetSQLConnStr()})
-			if err != nil {
-				t.Fatal(err)
-			}
-			db := userRepository{
-				db: gormDB,
-			}
 			if err = db.CreateUser(tt.args.user); err != nil {
 				t.Errorf("CreateUser() error = %v, wantErr %v", err, tt.wantErr)
 			}
