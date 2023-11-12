b6b523a0c5f9849c333ecc73a47ecbe3338dc5b1
ekovv <denisovdima2004@yandex.ru>
2023-11-12 17:51:30 +0300

    GOMARKET-11 :sparkles: with tests ✅

--- internal/controller/controller_test.go
+++ internal/controller/controller_test.go
@@ -111,7 +111,6 @@ func Test_userController_Login(t *testing.T) {
 			wantCode: http.StatusUnauthorized,
 		},
 	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			e := echo.New()
